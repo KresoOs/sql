3cc7dfd692d150d0a1c9fbf06fce3d9e6d94be62
Krešimir <ilead7@gmail.com>
2023-12-26 10:39:41 +0100

    dovrseni alter table

--- osnovnaskola.sql
+++ osnovnaskola.sql
@@ -8,4 +8,25 @@ use osnovnaskola;
 create table djeca(
 sifra int not null primary key identity(1,1),
 ime varchar(50),
+prezime varchar(50),
+
 );
+create table radionice(
+sifra int not null primary key identity(1,1),
+Ime varchar(50),
+uciteljica int,
+);
+create table uciteljice(
+sifra int not null primary key identity(1,1),
+ime varchar(50),
+prezime varchar(50),
+);
+create table radionice_djeca(
+sifra int not null primary key identity(1,1),
+dijete int,
+radionica int,
+
+);
+alter table radionice_djeca add foreign key (dijete) references djeca(sifra);
+alter table radionice_djeca add foreign key (radionica) references radionice(sifra);
+alter table radionice add foreign key (uciteljica) references uciteljice(sifra);
