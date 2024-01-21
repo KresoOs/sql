c898f3be3377bc5234975643c714000fdfdf5458
Krešimir <ilead7@gmail.com>
2024-01-21 13:46:36 +0100

    Skripta unijeti podaci

--- završni_rad/reklamacijski_pult.sql
+++ završni_rad/reklamacijski_pult.sql
@@ -10,6 +10,7 @@ create table djelatnici(
 	sifra int not null primary key identity(1,1),
 	ime varchar (50),
 	prezime varchar (50),
+
 );
 create table proizvodi(
 	sifra int not null primary key identity(1,1),
@@ -24,6 +25,30 @@ create table kupci (
 	prezime varchar (50),
 	telefon varchar (50),
 	email varchar (100),
+
+);
+
+create table reklamacije (
+	sifra int not null primary key identity(1,1),
+	kupac int,
+	datum date,
+
+);
+
+create table radninalozi (
+	sifra int not null primary key identity(1,1),
+	proizvod int,
+	reklamacija int,
+
+);
+
+create table statusi (
+	sifra int not null primary key identity(1,1),
+	radninalog int,
+	djelatnik int,
+	naziv varchar(50),
+	napomena varchar(150),
+
 );
 
 insert into djelatnici (ime,prezime)
@@ -44,3 +69,21 @@ insert into kupci (ime,prezime,telefon,email)
 	('Krešimir','Bilokapić','099/214-1409','ilead7@gmail.com'),
 	('Danijel','Bjelajac','098/169-8828','ulead7@gmail.com'),
 	('Duje','Garić','095/300-600','elead7@gmail.com');
+
+insert into statusi (naziv)
+	values
+		('Zaprimljeno'),
+		('Poslano u servis'),
+		('Zaprimljeno u servisu'),
+		('Dijagnostika'),
+		('Servis završen'),
+		('Poslano u poslovnicu'),
+		('Zaprimljeno u poslovnici'),
+		('Kupac obaviješten'),
+		('Izdano');
+
+alter table radninalozi add foreign key (proizvod) references proizvodi (sifra);
+alter table radninalozi add foreign key (reklamacija) references reklamacije (sifra);
+alter table statusi add foreign key (radninalog) references radninalozi (sifra);
+alter table statusi add foreign key (djelatnik) references djelatnici (sifra);
+alter table reklamacije add foreign key (kupac) references kupci (sifra);
